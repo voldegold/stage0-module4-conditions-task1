f745dacbb734fd101ee613ff28d3f8fe4b8a83a2
voldegold <kva1107@gmail.com>
2022-07-31 23:37:29 +0600

    okay

--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalFalsePrinter.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalFalsePrinter {
     public static void main(String[] args) {
+        int i = 1;
+        if (false) {
+            System.out.println(true);
+        }
+            else System.out.println( false );
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task1/ConditionalTruePrinter.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task1;
 
 public class ConditionalTruePrinter {
     public static void main(String[] args) {
+        int i = 1;
+        if (true) {
+            System.out.println( "true" );
+        }  else System.out.println( false );
     }
 }
